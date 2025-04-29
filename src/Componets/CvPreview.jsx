f480950872f4ae98e3270f6c5a61c074542fef65
Ahmad Rehman <ahmadrehman851@gamil.com>
2025-04-29 18:54:21 +0500

    Changed hard coded avatar img to the uploaded img using prop.image

--- src/Componets/CvPreview.jsx
+++ src/Componets/CvPreview.jsx
@@ -1,5 +1,6 @@
-import avatar from "../assets/avatarPlaceholder.png";
+import { v4 as uuidv4 } from "uuid";
 export default function CvPreview(props) {
+  console.log("props", props);
   return (
     <section className="live-preview-section">
       <header>
@@ -9,9 +10,9 @@ export default function CvPreview(props) {
       <main>
         <section className="contact-info">
           <div className="img-container">
-            <img src={avatar} alt="profile image" />
+            <img src={props.image} alt="profile image" />
           </div>
-          <div>
+          <div className="contact-info-container">
             <h2>CONTACT</h2>
             <ul>
               <li>{props.data.phoneNumber}</li>
@@ -19,8 +20,45 @@ export default function CvPreview(props) {
               <li>{props.data.address}</li>
             </ul>
           </div>
+          <div className="skills-container">
+            <h2>SKILLS</h2>
+            <ul>
+              {props.skillForm.map((skill) => {
+                return <li key={uuidv4()}>{skill.skillName}</li>;
+              })}
+            </ul>
+          </div>
         </section>
-        <section className="academic-experience-section"></section>
+        <section className="academic-experience-section">
+          <h2>EDUCATION</h2>
+          <div>
+            {props.educationForm.map((education) => {
+              return (
+                <div key={uuidv4()}>
+                  <h3>{education.schoolName}</h3>
+                  <p>{education.degree}</p>
+                  <p>{education.startDate}</p>
+                  <p>{education.endDate}</p>
+                </div>
+              );
+            })}
+          </div>
+          <h2>EXPERIENCE</h2>
+          <div>
+            {props.experienceForm.map((experience) => {
+              return (
+                <div key={uuidv4()}>
+                  <h3>{experience.companyName}</h3>
+                  <p>{experience.positionTitle}</p>
+                  <p>{experience.startDate}</p>
+                  <p>{experience.endDate}</p>
+                  <p>{experience.location}</p>
+                  <p>{experience.description}</p>
+                </div>
+              );
+            })}
+          </div>
+        </section>
       </main>
     </section>
   );
