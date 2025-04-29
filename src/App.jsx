427da541da4e8abc4dca2ba7a3fdb682c26fa3f2
Ahmad Rehman <ahmadrehman851@gamil.com>
2025-04-29 12:12:33 +0500

    Added Education form and its basic logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import CvPreview from "./Componets/CvPreview";
+import FormComponets from "./Componets/FormComponets";
 import "./App.css";
 import PersonalDetailForm from "./Componets/PersonalDetailForm";
 export default function App() {
@@ -11,18 +12,56 @@ export default function App() {
     address: "London, UK",
   });
 
+  const [educationForm, setEducationForm] = React.useState([
+    {
+      schoolName: "Goverment College university of Faisalabad",
+      degree: "BS(CS)",
+      startDate: "2-6-2021",
+      endDate: "2-6-2025",
+    },
+  ]);
+
   function handelFormChange(e) {
     const { name, value } = e.target;
     setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
   }
-  console.log(formData);
+  function handleEducationForm(e) {
+    e.preventDefault();
+    const { schoolName, degree, startDate, endDate } = e.target;
+
+    setEducationForm((prev) => {
+      return [
+        ...prev,
+        {
+          schoolName: schoolName.value,
+          degree: degree.value,
+          startDate: startDate.value,
+          endDate: endDate.value,
+        },
+      ];
+    });
+    // setIsFormShown((prevState) => !prevState);
+    // setIsEducationListShown((prevState) => !prevState);
+  }
+
+  console.log(educationForm);
   return (
     <>
       <main>
         <section className="form-section">
           <PersonalDetailForm handelFormChange={handelFormChange} />
+          <FormComponets
+            educationForm={educationForm}
+            handleEducationForm={handleEducationForm}
+            name="Education"
+          />
+          <FormComponets
+            educationForm={educationForm}
+            handleEducationForm={handleEducationForm}
+            name="Skills"
+          />
         </section>
-        <CvPreview data={formData} />
+        <CvPreview data={formData} educationForm={educationForm} />
       </main>
     </>
   );
