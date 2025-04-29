3f5c43b91b4cc758fc9f17dd30a9f49b6fcc5fe0
Ahmad Rehman <ahmadrehman851@gamil.com>
2025-04-29 17:25:08 +0500

    made reuseable component for skill,education,experience forms

---
+++ src/Componets/FormComponets.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+export default function FormComponets(props) {
+  const [isFormShown, setIsFormShown] = React.useState(false);
+  const [isListShown, setListShown] = React.useState(false);
+  if (isFormShown && isListShown) {
+    setIsFormShown((prevState) => !prevState);
+    setListShown((prevState) => !prevState);
+  }
+  function AddEntry() {
+    setIsFormShown((prevState) => !prevState);
+    setListShown((prevState) => !prevState);
+  }
+  function renderList() {
+    setListShown((prevState) => !prevState);
+  }
+  return (
+    <>
+      <div className="form">
+        <div className="formHeading" onClick={renderList}>
+          <h1>{props.name}</h1>
+          {isListShown ? (
+            <IoMdArrowDropup className="toggle-icon" />
+          ) : (
+            <IoMdArrowDropdown className="toggle-icon" />
+          )}
+        </div>
+      </div>
+      {isListShown && (
+        <>
+          {props.listItems}
+          <button onClick={AddEntry} className="add-education">
+            {props.name} +
+          </button>
+        </>
+      )}
+
+      {isFormShown && (
+        <>
+          <props.form handleFormSubmition={props.handleFormSubmition} />
+          <button onClick={AddEntry}>Close</button>
+        </>
+      )}
+    </>
+  );
+}
